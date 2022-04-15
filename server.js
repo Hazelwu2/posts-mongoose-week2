2d2894f80852107cc1443097efa2a9d5182cc319
Hazel Wu <tinazx056@gmail.com>
2022-04-16 00:18:08 +0800

    feat: server.js 新增 PORT, running 提示

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const http = require('http')
 const app = require('./app')
 const connectDB = require('./db.js')
+const PORT = process.env.PORT || 3005
 
 // 連接資料庫
 connectDB()
 
 const server = http.createServer(app)
-server.listen(process.env.PORT || 3005)
+server.listen(PORT, () => {
+  console.log(`Server is runnging at locahost:${PORT}`)
+})
